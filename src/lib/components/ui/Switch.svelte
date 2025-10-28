46fc37e7b324e7341a5d244c6dd4118499ef5794
BurakYs <brkyhs0@gmail.com>
2025-10-28 10:26:35 +0300

    fix: style inconsistency in Switch component

--- src/lib/components/ui/Switch.svelte
+++ src/lib/components/ui/Switch.svelte
@@ -14,17 +14,16 @@
   bind:checked
   {...restProps}
   class={cn(
-    'peer inline-flex items-center h-6 w-12 shrink-0 cursor-pointer rounded-full transition-colors',
-    'focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2',
-    'disabled:cursor-not-allowed disabled:opacity-50',
-    'focus-visible:ring-ring focus-visible:ring-offset-background',
-    'data-[state=checked]:bg-primary data-[state=unchecked]:bg-input'
+    'peer inline-flex items-center h-6 min-h-6 w-12 shrink-0 cursor-pointer rounded-full px-1 transition-colors',
+    'data-[state=checked]:bg-primary data-[state=unchecked]:bg-input',
+    'transition-colors focus-visible:ring-foreground focus-visible:ring-offset-background focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-offset-2',
+    'disabled:cursor-not-allowed disabled:opacity-50'
   )}
 >
   <Switch.Thumb
     class={cn(
-      'peer inline-block size-5 rounded-full transition-transform',
-      'data-[state=checked]:translate-x-6 data-[state=unchecked]:translate-x-1',
+      'pointer-events-none block size-5 shrink-0 rounded-full transition-transform',
+      'data-[state=checked]:translate-x-5 data-[state=unchecked]:translate-x-0 border data-[state=unchecked]:border',
       'data-[state=checked]:bg-background data-[state=unchecked]:bg-primary'
     )}
   />
